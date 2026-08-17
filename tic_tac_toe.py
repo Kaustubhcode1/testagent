9e7ade8bf77937bfd09fba65c8ca0f31975bac3b
Kaustubhcode1 <kaustubh.bhor@nitorinfotech.com>
2026-08-17 11:48:51 +0530

    testing1 to agent git automation specialist desription

---
+++ tic_tac_toe.py
@@ -0,0 +1,7 @@
+def display_board(board):
+    """
+    Prints the Tic-Tac-Toe board to the console.
+    The board is represented as a list of 9 strings.
+    """
+    print("\n" + "---" * 3)
+    print(f" {board[0]} | {board[1]} | {board[2]}
